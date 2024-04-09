6d769bb20c6ba94310b9be1c0605bbafd6ce169d
MarharytaFilipovych <150688071+MarharytaFilipovych@users.noreply.github.com>
2024-04-09 09:44:26 +0300

    gitignore

--- additional_to_task2.sql
+++ additional_to_task2.sql
@@ -1,7 +1,10 @@
-/*CREATE TABLE trail_imaginary_index AS
-SELECT * FROM trail_imaginary;*/
-/*CREATE INDEX index_trail
-    ON trail_imaginary_index (name);*/
+CREATE TABLE trail_imaginary_index AS
+SELECT * FROM trail_imaginary;
+
+
+CREATE INDEX index_trail
+    ON trail_imaginary_index (name);
+
 SELECT * FROM trail_imaginary
 WHERE name = 'Deer 2054';
 
